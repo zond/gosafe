31c918ad61db38382b75abb80decaecce9bef371
Martin Bruse <martin@oort.se>
2012-06-16 23:56:48 +0200

    more readme

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ To enable running any piece of Go code (even if it comes from unknown sources) w
 Use `Compiler.Allow` to allow given packages, then run code with `Compiler.Run` or `Compiler.RunFile`.
 
 See https://github.com/zond/gosafe/blob/master/examples/example.go
+
+## More
+
+Use https://github.com/zond/gosafety to communicate with the child processes. See https://github.com/zond/gosafe/blob/master/testfiles/test3.go for an example.
--- testfiles/test3.go
+++ testfiles/test3.go
@@ -1,24 +1,21 @@
-
+/*
+ * cmd, _ := c.RunFile(f)
+ * outj := gosafety.NewJSONWriter(cmd.Stdin)
+ * inj := gosafety.NewJSONReader(cmd.Stdout)
+ * json_from_process := <- inj
+ * outj <- my_json_response
+ */
 package main
 
 import (
 	"github.com/zond/gosafety"
-	"os"
-	"fmt"
 )
 
 func main() {
-	fmt.Fprintln(os.Stderr, "test3: 1")
 	json_in := gosafety.Stdin().PacketReader().JSONReader()
-	fmt.Fprintln(os.Stderr, "test3: 2")
 	json := (<-json_in).(map[string]interface{})
-	fmt.Fprintln(os.Stderr, "test3: 3")
 	json["returning"] = true
-	fmt.Fprintln(os.Stderr, "test3: 4")
 	json_out := gosafety.Stdout().PacketWriter().JSONWriter()
-	fmt.Fprintln(os.Stderr, "test3: 5")
 	json_out <- json
-	fmt.Fprintln(os.Stderr, "test3: 6")
 	<-json_in
-	fmt.Fprintln(os.Stderr, "test3: 7")
 }
