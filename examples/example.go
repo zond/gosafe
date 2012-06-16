fc35cebedbed360ab918112f17ec48cb7b406f89
Martin Bruse <martin@oort.se>
2012-06-16 18:28:58 +0200

    working example

--- examples/example.go
+++ examples/example.go
@@ -9,11 +9,12 @@ import (
 func main() {
 	c := gosafe.NewCompiler()
 	c.Allow("math")
-	cmd, err := c.Run("package main\nimport (\n\"fmt\"\n\"math\"\n)\nfunc main() { fmt.Print(math.Sin(10)) }\n")
+	cmd, err := c.Run("package main\nimport (\n\"fmt\"\n\"math\"\n)\nfunc main() { fmt.Println(math.Sin(10)) }\n")
 	fmt.Println(cmd, ", ", err)
 	c.Allow("fmt")
-	cmd, err = c.Run("package main\nimport (\n\"fmt\"\n\"math\"\n)\nfunc main() { fmt.Print(math.Sin(10)) }\n")
+	cmd, err = c.Run("package main\nimport (\n\"fmt\"\n\"math\"\n)\nfunc main() { fmt.Println(math.Sin(10)) }\n")
 	fmt.Println(cmd, ", ", err)
+	close(cmd.Stdin)
 	for b := range cmd.Stdout {
 		fmt.Print(string(b))
 	}
