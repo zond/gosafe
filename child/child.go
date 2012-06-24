d9388cd923dfaefed426affbf54874ad90f9f4c7
Martin Bruse <martin@oort.se>
2012-06-24 12:55:19 +0200

    more docu

--- child/child.go
+++ child/child.go
@@ -14,35 +14,41 @@ import (
 	"errors"
 )
 
-// Will produce a json.Decoder connected to os.Stdin
+// Stdin returns a *json.Decoder that decodes json from stdin.
 func Stdin() *json.Decoder {
 	return json.NewDecoder(os.Stdin)
 }
-// Will produce a json.Encoder connected to os.Stdout
+// Stdout returns a *json.Encoder that encodes json to stdout.
 func Stdout() *json.Encoder {
 	return json.NewEncoder(os.Stdout)
 }
 
+// These are the types of return data from a child.Server
 const (
 	Error = iota
 	Return
 	Callback
 )
 
+// NoSuchService is returned when there is no registered service of the wanted name.
 const NoSuchService = "No such service: %s"
 
+// Args is a shorthand for the array of interfaces used as arguments
 type Args []interface{}
 
+// Call is the type of data the Server expects.
 type Call struct {
 	Name string
 	Args Args
 }
 
+// Response is the type of data the Server outputs.
 type Response struct {
 	Type int
 	Payload interface{}
 }
 
+// Service is the type of function the Server can use to serve requests.
 type Service func(args... interface{}) interface{}
 func (self Service) callSafe(args... interface{}) (rval interface{}, err error) {
 	defer func() {
@@ -54,11 +60,14 @@ func (self Service) callSafe(args... interface{}) (rval interface{}, err error) 
 	return self(args...), nil
 }
 
+// Server serves requests by listening to Stdin, running Services matching the Name of incoming Calls, and responding
+// with their return values.
 type Server map[string]Service
 func (self Server) Register(name string, service Service) Server {
 	self[name] = service
 	return self
 }
+// Start the server and run it forever.
 func (self Server) Start() {
 	done := make(chan bool)
 	go self.serve(done)
@@ -92,6 +101,7 @@ func (self Server) serve(c chan bool) {
 	}
 }
 
+// Create a new Server.
 func NewServer() Server {
 	return make(Server)
 }
